05e78badfd049640d135df891a5a188544e670d4
Andriy Rukas <rukasav@gmail.com>
2024-01-30 13:01:48 +0200

    Refactor code for scalability

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,22 +1,23 @@
+from pages.result import DuckDuckGoResultPage
+from pages.search import DuckDuckGoSearchPage
 from playwright.sync_api import expect, Page
 
 
 def test_basic_duckduckgo_search(page: Page) -> None:
+    result_page = DuckDuckGoResultPage(page)
+    search_page = DuckDuckGoSearchPage(page)
+
     # Given the DuckDuckGo homepage displayed
-    page.goto("https://duckduckgo.com")
+    search_page.load()
 
     # When the user searches for a phrase
-    page.locator("id=searchbox_input").fill("panda")
-    page.locator("xpath=//button[@aria-label='Search']").click()
-    
+    search_page.search('panda')
+
     # Then the search result query is the phrase
-    expect(page.locator("//input[@id='search_form_input']")).to_have_value('panda')
-    
+    expect(result_page.search_input).to_have_value('panda')
+
     # And the search result links pertain to the phrase
-    page.locator("xpath=//a[@data-testid='result-title-a']").nth(4).wait_for()
-    titles = page.locator("xpath=//a[@data-testid='result-title-a']").all_text_contents()
-    matches = [t for t in titles if 'panda' in t.lower()]
-    assert len(matches) > 0
-    
+    assert result_page.result_link_titles_contain_phrase('panda')
+
     # And the search result title page contains the phrase
     expect(page).to_have_title('panda at DuckDuckGo')
