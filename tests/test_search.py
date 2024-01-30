53f7d40cae75818fbc6cf9b1c6e04cf7d10c4ba2
Andriy Rukas <rukasav@gmail.com>
2024-01-30 10:35:23 +0200

    Finish DuckDuckGo test case automation

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page
+from playwright.sync_api import expect, Page
 
 
 def test_basic_duckduckgo_search(page: Page) -> None:
@@ -10,6 +10,13 @@ def test_basic_duckduckgo_search(page: Page) -> None:
     page.locator("xpath=//button[@aria-label='Search']").click()
     
     # Then the search result query is the phrase
+    expect(page.locator("//input[@id='search_form_input']")).to_have_value('panda')
+    
     # And the search result links pertain to the phrase
-    # And the search result title contains the phrase
-    pass
+    page.locator("xpath=//a[@data-testid='result-title-a']").nth(4).wait_for()
+    titles = page.locator("xpath=//a[@data-testid='result-title-a']").all_text_contents()
+    matches = [t for t in titles if 'panda' in t.lower()]
+    assert len(matches) > 0
+    
+    # And the search result title page contains the phrase
+    expect(page).to_have_title('panda at DuckDuckGo')
